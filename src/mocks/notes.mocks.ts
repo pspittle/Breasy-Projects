fbb3e991f31051521be152e7915c16e102e94d30
Breasy <breasy@internode.on.net>
2018-05-17 16:34:55 +1000

    links

--- src/mocks/notes.mocks.ts
+++ src/mocks/notes.mocks.ts
@@ -1,12 +1,15 @@
 import { Notes} from '../models/notes.interface';
 
 const NotesList: Notes [] = [
-        {TopicNum: "2.1.4.2",NoteText: 'This is the first line of the notes.'},
-        {TopicNum: "2.1.4.2",NoteText: 'This is the 2nd line of the notes.'},
-        {TopicNum: "2.1.4.3",NoteText: 'This is the first line of the notes.'},
-        {TopicNum: "2.1.4.3",NoteText: 'This is the 2nd line of the notes.'},
-        {TopicNum: "2.1.4.3",NoteText: 'This is the 3rd line of the notes.'},
-        {TopicNum: "2.1.4.3",NoteText: 'This is the 4th line of the notes.'},
-];
+        {TopicNum: '2.1.1',NoteText: 'These services cannot extend to the determination of amounts to be recorded in the financial statements.'},
+        {TopicNum: '2.1.3',NoteText: 'Additionally, the Financial Condition Report acts as a high level health check on the entity.'},
+        {TopicNum: '2.1.4',NoteText: 'Provided (per PS 100) our report will be addressed to the auditor, there is written acceptance on appointment that copies of the report in its entirety will be provided to the Board, management and the Primary Actuary, and there is written acknowledgement on appointment that a copy of the report may be provided to APRA.'},
+        {TopicNum: '2.2.1.3',NoteText: 'Depending on the degree of reliance that will be placed on the particular IT systems as part of the audit, a determination shall be made as to whether to provide such non-assurance services only with personnel who are not members of the audit team and who have different reporting lines within the member firm.'},
+        {TopicNum: '2.2.4',NoteText: 'For example, providing estimated costs in cost optimization projects to source processes to another location.'},
+        {TopicNum: '2.2.4',NoteText: 'The source of the information must be communicated to management and the information should not be provided to third parties.'},
+        {TopicNum: '2.2.5',NoteText: 'We may only provide these services provided the client is actively involved, makes all decisions and does not delegate its responsibilities to a KPMG engagement team member.'},
+        {TopicNum: '2.2.5',NoteText: 'The documentation of a process does not include refining policies that may not be clearly developed by management.'},
+        {TopicNum: '2.2.5',NoteText: 'In instances where management’s policies are not clear or require refinement, we can provide recommendations to the client.'},
+                                ];
 
 export const NOTES_LIST = NotesList;
