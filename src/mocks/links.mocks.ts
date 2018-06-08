2bc7802c6ad6931c9cbb366766b13df6e6f5609d
Pete <petespitfire@gmail.com>
2018-06-08 13:53:33 +1000

    Link testing 2

--- src/mocks/links.mocks.ts
+++ src/mocks/links.mocks.ts
@@ -1,10 +1,10 @@
 import { Links} from '../models/links.interface';
 
 const LinksList: Links [] = [
-        {TopicNum: '0',LinkLabel: 'Link Testing', LinkURL: 'www.google.com.au'},
-        {TopicNum: '2.2.1.3',LinkLabel: 'QRM Manual - Audit Team Definition', LinkURL: 'www.google.com.au'},
-        {TopicNum: '2.2.3.1',LinkLabel: 'Risk Management Contacts', LinkURL: 'www.google.com.au'},
-        {TopicNum: '2.2.4.1',LinkLabel: 'Risk Management Contacts', LinkURL: 'www.google.com.au'},
+        {TopicNum: '0',LinkLabel: 'Link Testing', LinkURL: 'http://www.google.com.au'},
+        {TopicNum: '2.2.1.3',LinkLabel: 'QRM Manual - Audit Team Definition', LinkURL: 'http://www.google.com.au'},
+        {TopicNum: '2.2.3.1',LinkLabel: 'Risk Management Contacts', LinkURL: 'http://www.google.com.au'},
+        {TopicNum: '2.2.4.1',LinkLabel: 'Risk Management Contacts', LinkURL: 'http://www.google.com.au'},
         ];
 
 export const LINKS_LIST = LinksList;
